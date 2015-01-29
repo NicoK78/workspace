5f9a1fed43d0636d1cc04eda6d19207bfa93be4a
Nicolas <nicolas.kervoern@gmail.com>
2015-01-29 15:48:14 +0100

    Fin TP2

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Reviews = require('../database/Reviews');
 
 var _ = require('lodash');
 
+
+router.route('/')
+	.get(function (req, res) {
+		Reviews.find({}, function (err, reviews) {
+			if (err) {
+				res.status(500).send({'error': err});
+			} else {
+				res.status(200).send(reviews);				
+			}
+		});
+	})
+	.post(function (req, res) {
+		console.log("Test");
+		if(req.body.name && req.body.placeType && req.body.stars) {
+			Reviews.create(req.body, function (err, review) {
+				if (err) {
+					res.status(500).send({'error': err});
+				}
+				res.status(201).send(review);
+			});
+		}
+		else {
+			res.status(400).send("Worst Request Ever !");
+		}
+	})
+	.delete(function (req, res) {
+		Reviews.remove({}, function (err) {
+			res.status(200).send();
+		});
+	});
+
+
+router.route('/:id')
+	.get(function (req, res) {
+		Reviews.findOne({_id: req.params.id}, function (err, review) {
+			if (err) {
+				res.status(500).send({'error': err});
+			} else {
+				if (!review) {
+					res.status(404).send();
+				} else {
+					res.status(200).send(review);
+				}
+			}
+		});
+	})
+	.put(function(req, res) {
+		if(req.body.name && req.body.placeType && req.body.stars) {
+			Reviews.findByIdAndUpdate(req.params.id, req.body, function (err, review) {
+				if (err) {
+					res.status(500).send({'error': err});
+				} else {
+					res.status(200).send(review);
+				}
+			});
+		} else {
+			res.status(400).send("Worst Request Ever !");
+		}
+	})
+	.delete(function (req, res) {
+		Reviews.remove({_id: req.params.id}, function (err) {
+			if(err) {
+				res.status(400).send();
+			}
+			res.status(200).send();
+		});
+	});
+
+
+
+
+/*
 var reviews = [
 	{
 		name: 'McDo',
@@ -83,5 +157,7 @@ router.delete('/:id', function(req, res) {
 		res.status(202).send(reviews);
 	}
 });
+*/
+
 
 module.exports = router;
