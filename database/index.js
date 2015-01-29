82e0607b645cd28053c10dad80aeb71ae5f83b6f
Nicolas <nicolas.kervoern@gmail.com>
2015-01-29 16:38:12 +0100

    Connect Mongolab

--- database/index.js
+++ database/index.js
@@ -2,16 +2,11 @@
 
 var mongoose = require('mongoose');
 
-var mongolabStringConnexion = 'mongodb://localhost:27017/ws-restapi';
+var mongolabStringConnexion = process.env.MONGO_STRING_CONNECTION || 'mongodb://localhost:27017/ws-restapi';
 
 mongoose.connect(mongolabStringConnexion);
-
 var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function callback () {
-
-	console.log('Connexion establish to ' + mongolabStringConnexion);
-
+    console.log('Connexion establish to ' + mongolabStringConnexion);
 });
