c5afe6438581ac2253a9155dbdcd7fde9254f39e
Joan Ortega <jomaora@gmail.com>
2015-03-14 19:57:42 +0100

    rewriting 9e000981065a40fb3fdbac579565f72c30d89436 without node_modules

--- routes/reviews.js
+++ routes/reviews.js
@@ -22,43 +22,66 @@ var reviews = [
 ];
 
 router.get('/', function(req, res, next) {
-	res.send(reviews);
-	//res.render('reviews', {title: 'Reviews', reviewsTab: reviews});
+	if(!reviews[0]) {
+		res.status(404).send();
+	} else {
+		res.status(200).send(reviews);
+		//res.render('reviews', {title: 'Reviews', reviewsTab: reviews});
+	}
 });
 
 router.post('/', function(req, res) {
-	reviews.push(
-		{
+	if(req.body.name && req.body.placeType && req.body.stars) {
+		reviews.push({
 			name: req.body.name, 
 			placeType: req.body.placeType, 
 			stars: req.body.stars
-		}
-	);
-	res.send(reviews);
+		});
+		res.status(201).send(reviews);
+	} else {
+		res.status(400).send("Worst Request Ever !");
+	}
 });
 
 router.delete('/', function(req, res) {
-	reviews = [];
-	res.send(reviews);
+	if(!reviews[0]) {
+		res.status(404).send("Empty Array !");
+	} else {
+		reviews = [];
+		res.status(202).send(reviews);
+	}
 });
 
 router.get('/:id', function(req, res, next) {
-	var id = req.params.id;
-	res.send(reviews[id]);
-})
+	if(!reviews[id]) {
+		res.status(404).send();
+	} else {
+		var id = req.params.id;
+		res.status(200).send(reviews[id]);
+	}
+});
 
 router.put('/:id', function(req, res) {
-	var id = req.params.id;	
-	reviews[id].name = req.body.name;
-	reviews[id].placeType = req.body.placeType; 
-	reviews[id].stars = req.body.stars;
-	res.send(reviews);
-})
+	if(req.body.name && req.body.placeType && req.body.stars) {
+		var id = req.params.id;	
+		reviews[id].name = req.body.name;
+		reviews[id].placeType = req.body.placeType; 
+		reviews[id].stars = req.body.stars;
+		res.status(200).send(reviews);
+	} else {
+		res.status(400).send("Worst Request Ever !");
+	}
+});
 
 router.delete('/:id', function(req, res) {
 	var id = req.params.id;
-	_.pullAt(reviews, id);
-	res.send(reviews);
+	console.log(reviews[id]);
+	if(!reviews[id]) {
+		res.status(404).send();
+	} else {
+		_.pullAt(reviews, id);
+		res.status(202).send(reviews);
+	}
 });
 
 module.exports = router;
